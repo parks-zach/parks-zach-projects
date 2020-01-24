ba94f83dbeced9e2d11d35a7f97f0d0181389316
parks-zach <zach.parks@hotmail.com>
2020-01-24 02:48:04 -0500

    Add files via upload

---
+++ Projects/CSCI_DB.sql
@@ -0,0 +1,11 @@
+spool /USERS/zach/Desktop/lab
+describe students
+insert into students (snumber,fname,lname,pnumber) values ('000000000','Zachary','Parks','3176509933');
+insert into students (snumber,fname,lname,pnumber) values ('000000001','Justin','Blakely','3171234567');
+insert into students (snumber,fname,lname,pnumber) values ('000000002','Justin','Carlton','3172345678');
+insert into students (snumber,fname,lname,pnumber) values ('000000003','Taylor','Hurley','3173456789');
+select *from students;
+describe grades;
+
+spool off
+
